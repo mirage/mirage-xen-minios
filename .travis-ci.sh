68a0a25adbfb5868ad68b60d1dc19583304f73f4
Anil Madhavapeddy <anil@recoil.org>
2014-07-05 22:18:03 +0100

    travis: also test opam installation

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,20 @@
 sudo apt-get install -qq build-essential pkg-config
 env SUDO=sudo ./install.sh
 env PKG_CONFIG_PATH=/usr/local pkg-config --list-all
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+opam init
+./install.sh
