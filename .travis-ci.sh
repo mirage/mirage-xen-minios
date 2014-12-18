b15418630c82b6aae5783a86d54b3d11779d94bb
Thomas Leonard <talex5@gmail.com>
2014-12-18 17:00:57 +0000

    Travis: test we can build mirage-xen

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,9 @@
+#!/bin/sh
+set -eux
 sudo apt-get install -qq build-essential pkg-config
-env SUDO=sudo make all
-env PKG_CONFIG_PATH=/usr/local pkg-config --list-all
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
@@ -17,4 +14,7 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 opam init
-make all
+
+opam pin -k git add mirage-xen-minios .
+opam install mirage-xen-minios
+opam install mirage-xen
