11d89f4955ea51839ecb86ac7538ec328a5f49ee
Anil Madhavapeddy <anil@recoil.org>
2015-03-08 21:28:54 +0000

    Attempt to reproduce the build failure in mirage/mirage-console#36

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,6 +4,7 @@ sudo apt-get install -qq build-essential pkg-config
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
@@ -15,6 +16,12 @@ export OPAMYES=1
 export OPAMVERBOSE=1
 opam init
 
+opam install depext
+eval `opam config env`
+
 opam pin -k git add mirage-xen-minios .
 opam install mirage-xen-minios
 opam install mirage-xen
+
+opam depext xenctrl
+opam reinstall mirage-xen-minios
