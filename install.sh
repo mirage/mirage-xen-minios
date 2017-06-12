caf5c20458dd3513603c4aabf1e2ba669f7cc57c
David Scott <dave@recoil.org>
2017-06-12 10:53:23 +0100

    openlibm installs in include/openlibm
    The bundled openlibm.pc file sets the includedir to /include.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@
 rm -rf ${LIBM}
 tar -zxf ${LIBM_ARCHIVE}
 cd ${LIBM}
+patch -p1 < ../patch-1.diff
 make CFLAGS="$CFLAGS $(PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig pkg-config libminios-xen --cflags)" libopenlibm.a
 ${SUDO} make install prefix=${PREFIX}
